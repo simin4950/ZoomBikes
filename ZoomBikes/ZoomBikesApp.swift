1fa3852e24373995af011b688da758c247a30965
simin4950@gmail.com <capstone@NMIs-MacBook-Pro.local>
2020-10-03 22:13:58 -0400

    Intial files for Zoom Bikes

---
+++ ZoomBikes/ZoomBikesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomBikesApp.swift
+//  ZoomBikes
+//
+//  Created by Capstone on 10/3/20.
+//
+
+import SwiftUI
+
+@main
+struct ZoomBikesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
