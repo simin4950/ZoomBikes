df808d2d3a8984aa29f0f5b7c65b885283a5f785
simin4950@gmail.com <capstone@NMIs-MacBook-Pro.local>
2020-10-08 18:46:35 -0400

    Fixed Navigation bugs in App Clip

--- ZoomBikesAppClip/SafetyInstructions.swift
+++ ZoomBikesAppClip/SafetyInstructions.swift
@@ -9,58 +9,58 @@ import SwiftUI
 
 struct SafetyInstructions: View {
     var body: some View {
-        ZStack{
-            Color.black
-                .ignoresSafeArea()
-            VStack{
-                Image("bike")
-                Text("How it Works:")
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-                HStack{
-                    Image("check")
-                    Text("Find a bike")
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                        .foregroundColor(.white)
-                }
-                HStack{
-                    Text("Use ")
-                        .font(.title)
-                        .foregroundColor(.white)
-                    Image("apple")
-                    Text(" Pay")
-                        .font(.title)
-                        .foregroundColor(.white)
-                }
-                Text("Unlock + Ride")
-                    .font(.title)
-                    .foregroundColor(.white)
-                Text("")
-                Text("Lock it up in")
-                    .font(.title)
-                    .foregroundColor(.white)
-                Text("a bike zone")
-                    .font(.title)
-                    .foregroundColor(.white)
-                Spacer()
+        NavigationView {
+            ZStack{
+                Color.black
+                    .ignoresSafeArea()
                 VStack{
-                    NavigationLink(
-                        destination: MapViewer(),
-                        label: {
-                            Text("LET'S GO")
-                                .padding()
-                                .padding(.horizontal, 50)
-                                .font(.largeTitle)
-                                .foregroundColor(.black)
-                                .background(Color.pink)
-                                .cornerRadius(50)
-                        })
+                    Image("bike")
+                    Text("How it Works:")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                    HStack{
+                        Image("check")
+                        Text("Find a bike")
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                    }
+                    HStack{
+                        Text("Use ")
+                            .font(.title)
+                            .foregroundColor(.white)
+                        Image("apple")
+                        Text(" Pay")
+                            .font(.title)
+                            .foregroundColor(.white)
+                    }
+                    Text("Unlock + Ride")
+                        .font(.title)
+                        .foregroundColor(.white)
+                    Text("")
+                    Text("Lock it up in")
+                        .font(.title)
+                        .foregroundColor(.white)
+                    Text("a bike zone")
+                        .font(.title)
+                        .foregroundColor(.white)
+                    Spacer()
+                    VStack{
+                        NavigationLink(
+                            destination: MapViewer(),
+                            label: {
+                                Text("LET'S GO")
+                                    .padding()
+                                    .padding(.horizontal, 50)
+                                    .font(.largeTitle)
+                                    .foregroundColor(.black)
+                                    .background(Color.pink)
+                                    .cornerRadius(50)
+                            })
+                    }
+                    
                 }
-                
             }
         }
-        
-        
     }
 }
 
